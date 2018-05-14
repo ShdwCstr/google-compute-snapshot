14c025b3d1c2dfa9c91dcd1093944a118b58a020
Dan Feagan <dan@changegamers.com>
2018-05-14 13:59:18 -0700

    MOD updated for different times and time spans

--- gcloud-snapshot.sh
+++ gcloud-snapshot.sh
@@ -19,14 +19,40 @@ export PATH=$PATH:/usr/local/bin/:/usr/bin
 #
 
 usage() {
-  echo -e "\nUsage: $0 [-d <days>]" 1>&2
+  echo -e "\nUsage: $0 [-d <days>] [-p <prefix>]" 1>&2
   echo -e "\nOptions:\n"
   echo -e "    -d    Number of days to keep snapshots.  Snapshots older than this number deleted."
   echo -e "          Default if not set: 7 [OPTIONAL]"
   echo -e "\n"
+  echo -e "    -h    Number of hours to keep snapshots.  Snapshots older than this number deleted."
+  echo -e "          Default if not set: 5 [OPTIONAL]"
+  echo -e "\n"
+  echo -e "    -m    Number of months to keep snapshots.  Snapshots older than this number deleted."
+  echo -e "          Default if not set: 3 [OPTIONAL]"
+  echo -e "\n"
+  echo -e "    -p    Prefix for snapshot name."
+  echo -e "          Default is no prefix [OPTIONAL]"
+  echo -e "\n"
+  echo -e "    -w    Number of weeks to keep snapshots.  Snapshots older than this number deleted."
+  echo -e "          Default if not set: 2 [OPTIONAL]"
+  echo -e "\n"
+  echo -e "    -y    Number of years to keep snapshots.  Snapshots older than this number deleted."
+  echo -e "          Default if not set: 2 [OPTIONAL]"
+  echo -e "\n"
   exit 1
 }
 
+#
+# SOME DEFAULTS, BASED ON GIVENOPTION, FOR $OLDER_THAN
+#
+TIMEZONE_OFFSET="-7 hours";
+DEFAULT_PREFIX="daily-"
+DEFAULT_TIMEFRAME="days"
+DEFAULT_OLDERTHAN_DAYS=7
+DEFAULT_OLDERTHAN_HOURS=5
+DEFAULT_OLDERTHAN_WEEKS=2
+DEFAULT_OLDERTHAN_MONTHS=3
+DEFAULT_OLDERTHAN_YEARS=2
 
 #
 # GETS SCRIPT OPTIONS AND SETS GLOBAL VAR $OLDER_THAN
@@ -34,10 +60,51 @@ usage() {
 
 setScriptOptions()
 {
-    while getopts ":d:" o; do
+    #  We use days as default if not options are give, so set a default
+	OLDER_THAN=$DEFAULT_OLDERTHAN_DAYS
+    PREFIX=$DEFAULT_PREFIX"daily-"
+    TIMEFRAME=$DEFAULT_TIMEFRAME"days"
+	CUSTOM_PREFIX=""
+
+    while getopts ":d:h:m:p:w:y:" o; do
       case "${o}" in
         d)
-          opt_d=${OPTARG}
+          param=${OPTARG}
+          OLDER_THAN=$DEFAULT_OLDERTHAN_DAYS
+          PREFIX="daily-"
+          TIMEFRAME="days"
+          ;;
+
+        h)
+          param=${OPTARG}
+          OLDER_THAN=$DEFAULT_OLDERTHAN_HOURS
+          PREFIX="hourly-"
+          TIMEFRAME="hours"
+          ;;
+
+        m)
+          param=${OPTARG}
+          OLDER_THAN=$DEFAULT_OLDERTHAN_MONTHS
+          PREFIX="monthly-"
+          TIMEFRAME="months"
+          ;;
+
+        p)
+          CUSTOM_PREFIX=${OPTARG}"-"
+          ;;
+
+        w)
+          param=${OPTARG}
+          OLDER_THAN=$DEFAULT_OLDERTHAN_WEEKS
+          PREFIX="weekly-"
+          TIMEFRAME="weeks"
+          ;;
+
+        y)
+          param=${OPTARG}
+          OLDER_THAN=$DEFAULT_OLDERTHAN_YEARS
+          PREFIX="yearly-"
+          TIMEFRAME="years"
           ;;
 
         *)
@@ -47,11 +114,14 @@ setScriptOptions()
     done
     shift $((OPTIND-1))
 
-    if [[ -n $opt_d ]];then
-      OLDER_THAN=$opt_d
-    else
-      OLDER_THAN=7
+    logTime "Param was "${param}""
+
+	if [[ -n $param ]];then
+      OLDER_THAN=$param
     fi
+
+    PREFIX=${CUSTOM_PREFIX}${PREFIX}
+	logTime "Executing snapshot with Prefix(${PREFIX}), with delete Timeframe(-${OLDER_THAN} ${TIMEFRAME})"
 }
 
 
@@ -111,7 +181,7 @@ getDeviceList()
 createSnapshotName()
 {
     # create snapshot name
-    local name="gcs-$1-$2-$3"
+    local name="${PREFIX}gcs-$1-$2-$3"
 
     # google compute snapshot name cannot be longer than 62 characters
     local name_max_len=62
@@ -120,7 +190,7 @@ createSnapshotName()
     if [ ${#name} -ge ${name_max_len} ]; then
 
         # work out how many characters we require - prefix + device id + timestamp
-        local req_chars="gcs--$2-$3"
+        local req_chars="${PREFIX}gcs--$2-$3"
 
         # work out how many characters that leaves us for the device name
         local device_name_len=`expr ${name_max_len} - ${#req_chars}`
@@ -129,7 +199,7 @@ createSnapshotName()
         local device_name=${1:0:device_name_len}
 
         # create new (acceptable) snapshot name
-        name="gcs-${device_name}-$2-$3" ;
+        name="${PREFIX}gcs-${device_name}-$2-$3" ;
 
     fi
 
@@ -188,7 +258,7 @@ getSnapshotCreatedDate()
     local snapshot_datetime="$(gcloud compute snapshots describe $1 | grep "creationTimestamp" | cut -d " " -f 2 | tr -d \')"
 
     #  format date
-    echo -e "$(date -d ${snapshot_datetime%?????} +%Y%m%d)"
+    echo -e "$(date -d ${snapshot_datetime%?????} +%Y%m%d%H)"
     
     # Previous Method of formatting date, which caused issues with older Centos
     #echo -e "$(date -d ${snapshot_datetime} +%Y%m%d)"
@@ -203,7 +273,7 @@ getSnapshotCreatedDate()
 
 getSnapshotDeletionDate()
 {
-    echo -e "$(date -d "-$1 days" +"%Y%m%d")"
+    echo -e "$(date -d "-$1 ${TIMEFRAME} ${TIMEZONE_OFFSET}" +"%Y%m%d%H")"
 }
 
 
@@ -291,14 +361,17 @@ deleteSnapshotsWrapper()
     # get the deletion date for snapshots
     DELETION_DATE=$(getSnapshotDeletionDate "${OLDER_THAN}")
 
+	#logTime "DELETION_DATE is set to ${DELETION_DATE})"
+
     # get list of snapshots for regex - saved in global array
-    getSnapshots "gcs-.*${INSTANCE_ID}-.*"
+    getSnapshots "${PREFIX}gcs-.*${INSTANCE_ID}-.*"
 
     # loop through snapshots
     for snapshot in "${SNAPSHOTS[@]}"
     do
         # get created date for snapshot
         SNAPSHOT_CREATED_DATE=$(getSnapshotCreatedDate ${snapshot})
+		#logTime "SNAPSHOT_CREATED_DATE is set to ${SNAPSHOT_CREATED_DATE})"
 
         # check if snapshot needs to be deleted
         DELETION_CHECK=$(checkSnapshotDeletion ${DELETION_DATE} ${SNAPSHOT_CREATED_DATE})
